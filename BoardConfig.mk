f09ef048f39eef62b893feda8476c3564a677cdf
inferno0230 <mail@inferno0230.in>
2024-08-20 18:24:47 +0530

    Revert "aston: Provide a static lib for power mode extension"
    This reverts commit 8723907bde28aa009515fa1b9bf3fff2c7b7603e.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,9 +33,6 @@ BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVIC
 BOOT_KERNEL_MODULES := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery $(DEVICE_PATH)/modules.include.vendor_ramdisk))
 SYSTEM_KERNEL_MODULES := $(strip $(shell cat $(DEVICE_PATH)/modules.include.system_dlkm))
 
-# Power
-TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(DEVICE_PATH)/power:libperfmgr-ext-aston
-
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
